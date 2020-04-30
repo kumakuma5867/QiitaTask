546a0e4550d3166985b89c36f0d25846b61f5847
tk-shiraishi <takuma.shiraishi@lifematics.co.jp>
2020-04-30 10:08:26 +0900

    feat: Taskモデルを作成

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    class TaskStatus(models.IntegerChoices):
+        ADDED = 10
+        DOING = 20
+        PENDING = 30
+        DONE = 100
+
+    title = models.CharField('Title', max_length=50)
+    detail = models.TextField('Detail', max_length=500, null=True, blank=True)
+    deadline = models.DateField('Deadline', null=True, blank=True)
+    status = models.IntegerField('Status', choices=TaskStatus.choices, default=TaskStatus.ADDED)
+
+    def __str__(self):
+        return self.title
